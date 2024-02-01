22233f03d0a8b5f58b46f588c6ff5882d572d230
Stebbins <stebbi39@msu.edu>
2024-02-01 00:58:12 -0500

    reads in lake superior plot data

--- R/get_spatial_data.R
+++ R/get_spatial_data.R
@@ -2,6 +2,7 @@ library(readxl)
 library(tidyverse)
 library(here)
 library(janitor)
+library(ggplot2)
 
 # spatial analyses
 survey_metadata <- read_xlsx("data-raw/SuperiorSurveyData.xlsx",
@@ -9,7 +10,8 @@ survey_metadata <- read_xlsx("data-raw/SuperiorSurveyData.xlsx",
 
 surveys_with_centroids <- survey_metadata |> 
   clean_names() |>
-  distinct(location, year, latitude, longitude)
+  distinct(location, year, latitude, longitude) |> 
+  mutate(longitude = -1 * longitude)
 
 
 load("data-raw/incdat_2021.Rdata")
@@ -21,4 +23,27 @@ fish_ids_with_centroids <- idat |>
   distinct(id, latitude, longitude, location = Location, year_capture)
 
 save(fish_ids_with_centroids, file = "data/fish_ids_with_centroids.Rdata")
+
+
+# get individual net locations
+net_metadata <- read_xlsx("data-raw/SuperiorSurveyData.xlsx",
+                          sheet = "Sampling") |> 
+  clean_names()
+
+nets_with_lat_longs <- net_metadata |> 
+  mutate(longitude = -1 * longitude) |> 
+  distinct(location, survey_id, effort_id, latitude, longitude) |> 
+  glimpse()
+
+save(nets_with_lat_longs, file = "data/nets_with_lat_longs.Rdata")
+
+# read in outline of lake superior
+
+# Lake Superior: https://www.glerl.noaa.gov/data/bathy/bathy.html
+lake_superior_lat_longs <- read_csv("data-raw/LakeSuperior_latlong.csv") |> 
+  rename(longitude = `long...1`, latitude = `long...2`, location = Location) |> 
+  glimpse()
+
+save(lake_superior_lat_longs, file = "data/lake_superior_lat_longs.Rdata")
+
 
